717a6efc876979012fb4a3196629749cf996044c
Kayo <kayohsantos0@gmail.com>
2025-05-27 15:38:15 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿int[] arrayInteiros = new int[3];
+
+arrayInteiros[0] = 72;
+arrayInteiros[1] = 64;
+arrayInteiros[2] = 50;
+
+Console.WriteLine("Percorrendo o Array com o FOR");
+
+for (int contador = 0; contador < arrayInteiros.Length; contador++)
+{
+    Console.WriteLine($"Posição N° {contador} - {arrayInteiros[contador]}");
+}
+
+
+Console.WriteLine("Percorrendo o Array com FOREACH");
+
+int contadorForeach = 0;
+foreach (int valor in arrayInteiros)
+{
+    Console.WriteLine($"Possição N° {contadorForeach} - {valor}");
+    contadorForeach++;
+}
